750d00c852fd4200df59f6db7f7f9c7a382d864b
Matthew Ghosh <matthew.ghosh88@gmail.com>
2024-01-21 15:02:18 +0000

    Added blank tests

---
+++ src/test/java/mvg/solo/team/TeamTest.java
@@ -0,0 +1,57 @@
+package mvg.solo.team;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getTeams() {
+    }
+
+    @Test
+    void getTeam() {
+    }
+
+    @Test
+    void getRatingPoints() {
+    }
+
+    @Test
+    void updateRatingPoints() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void homeKitString() {
+    }
+
+    @Test
+    void alternateKitString() {
+    }
+
+    @Test
+    void countryName() {
+    }
+
+    @Test
+    void initialRatingPoints() {
+    }
+
+    @Test
+    void homeKit() {
+    }
+
+    @Test
+    void alternateKit() {
+    }
+}
